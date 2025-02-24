4a77029f4be3e6b2fe37c445578743166e62df80
kartikunnithan <kartikunnithan738@gmail.com>
2025-02-24 19:59:04 +0400

    3

---
+++ main3.py
@@ -0,0 +1,16 @@
+Math=100
+English=96
+science=98
+coding=96
+
+sum=Math+English+science+coding
+
+print("The sum of all values above is ", sum)
+
+average=sum/4
+
+print("The average of utems above is ", average)
+
+percentage=(sum/400)*100
+
+print("The percentage total for all subjects is ",percentage)
